2ba28474a25f3bf28e9d80b9cbb42cf7e95ea351
papum_debian <danieledugo1@gmail.com>
2023-04-27 10:32:55 +0200

    first commimt

---
+++ backend/src/controllers/notes.ts
@@ -0,0 +1,120 @@
+import { RequestHandler } from "express";
+import NoteModel from "../models/note";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
+
+export const getNotes: RequestHandler = async (req, res, next) => {
+	try {
+		const notes = await NoteModel.find().exec();
+		res.status(200).json(notes);
+	} catch(error) {
+		next(error);
+	}
+};
+	
+export const getNote: RequestHandler = async (req, res, next) => {
+	const noteId = req.params.noteId;
+	
+	try {
+		if(!mongoose.isValidObjectId(noteId)) {	// noteId != null, as it would go to / instead of /api/notes
+			throw createHttpError(400, "Invalid note id");
+		}
+		
+		const note = await NoteModel.findById(noteId).exec();
+
+		if(!note) {
+			throw createHttpError(404, "Note not found");
+		}
+
+		res.status(200).json(note);
+	} catch (error) {
+		next(error);
+	}
+}
+
+interface CreateNoteBody {
+	title?: string,		// required by schema, but want to thow exception myself in createNote
+	text?: string, 
+}
+
+export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknown> = async (req, res, next) => {
+	const title = req.body.title;
+	const text = req.body.text;
+	
+	try {
+		if(!title) {
+			throw createHttpError(400, "Note must have a title");
+		}
+
+		const newNote = await NoteModel.create({
+			title: title,
+			text: text,
+		});
+
+		res.status(201).json(newNote);
+	} catch (error) {
+		next(error);
+	}
+};
+
+interface UpdateNoteParams {
+	noteId: string,
+}
+
+interface UpdateNoteBody {
+	title?: string,
+	text?: string, 
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async(req, res, next) => {
+	const noteId = req.params.noteId;
+	const newTitle = req.body.title;
+	const newText = req.body.text;
+	
+	try {
+		if(!mongoose.isValidObjectId(noteId)) {	// noteId != null, as it would go to / instead of /api/notes
+			throw createHttpError(400, "Invalid note id");
+		}
+		if(!newTitle) {
+			throw createHttpError(400, "Note must have a title");
+		}
+
+		const note = await NoteModel.findById(noteId).exec();
+		
+		if(!note) {
+			throw createHttpError(404, "Note not found");
+		}
+		
+		note.title = newTitle;
+		note.text = newText;
+
+		const updatedNote = await note.save();
+
+		res.status(200).json(updatedNote);
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const deleteNote: RequestHandler = async(req, res, next) => {
+	const noteId = req.params.noteId;
+	
+	try {
+		if(!mongoose.isValidObjectId(noteId)) {	// noteId != null, as it would go to / instead of /api/notes
+			throw createHttpError(400, "Invalid note id");
+		}
+
+		const note = await NoteModel.findById(noteId).exec();
+		
+		if(!note) {
+			throw createHttpError(404, "Note not found");
+		}
+
+		await note.deleteOne();
+		
+		
+		res.sendStatus(204);
+	} catch (error) {
+		next(error);
+	}
+};
---
+++ backend/src/models/note.ts
@@ -0,0 +1,11 @@
+import { model } from "mongoose";
+import { InferSchemaType, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+	title: { type: String, required: true },
+	text: { type: String },
+}, { timestamps: true });
+
+type Note = InferSchemaType<typeof noteSchema>;
+
+export default model<Note>("Note", noteSchema);
